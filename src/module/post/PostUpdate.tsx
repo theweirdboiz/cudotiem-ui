8618be815b03fd8c0cf01b3a3e9a548cd44ccb20
theweirdboiz <19130227@st.hcmuaf.edu.vn>
2023-06-02 12:06:17 +0700

    test update post

--- src/module/post/PostUpdate.tsx
+++ src/module/post/PostUpdate.tsx
@@ -66,12 +66,12 @@ const PostUpdate = () => {
   useEffect(() => {
     document.title = 'Cụ Đồ Tiễm - Cập nhật tin đăng'
     const category = categories?.find((category) => category.name === post?.postDetailResponse.categoryName)
-    isSuccess && setCategorySelected(category)
-    isSuccess && setImageUrls(post?.postDetailResponse?.imageUrls || [])
-    isSuccess && setContent(post?.postDetailResponse.content || '')
-    isSuccess && handleChangeThumbnail(post?.postDetailResponse?.imageUrls?.[0] || '')
-    isSuccess && handleChangeImageUrls(post?.postDetailResponse?.imageUrls?.filter((img) => img !== thumbnail) || [])
-    isSuccess && reset(post?.postDetailResponse)
+    setCategorySelected(category)
+    setImageUrls(post?.postDetailResponse?.imageUrls || [])
+    setContent(post?.postDetailResponse.content || '')
+    handleChangeThumbnail(post?.postDetailResponse?.imageUrls?.[0] || '')
+    handleChangeImageUrls(post?.postDetailResponse?.imageUrls?.filter((img) => img !== thumbnail) || [])
+    reset(post?.postDetailResponse)
   }, [isSuccess])
 
   const handleEditorChange = (content: string) => {
@@ -133,129 +133,137 @@ const PostUpdate = () => {
 
   return (
     <>
-      <DashboardHeading>Cập nhật tin đăng</DashboardHeading>
-      <form action='' onSubmit={handleSubmit(onSubmit)}>
-        <div className='grid grid-cols-2 gap-3'>
-          <FormGroup>
-            <Label className={`${errors.title && 'text-red-400'}`}>Tiêu đề</Label>
-            <Input control={control} placeholder='Tiêu đề tin đăng' name='title' error={errors.title?.message} />
-          </FormGroup>
-          <FormGroup>
-            <Label>Danh mục</Label>
-            <Dropdown>
-              <Dropdown.Select placeholder={categorySelected ? categorySelected.name : 'Chọn danh mục'} />
-              <Dropdown.List>
-                {categories?.map((item: Category) => (
-                  <Dropdown.Option option={item} key={item.id} onClick={handleClickOption}>
-                    {item.name}
-                  </Dropdown.Option>
-                ))}
-              </Dropdown.List>
-              <span></span>
-            </Dropdown>
-          </FormGroup>
-          {auth?.roles.includes(Role.ADMIN || Role.MODERATOR) && (
+      {isSuccess && (
+        <>
+          <DashboardHeading>Cập nhật tin đăng</DashboardHeading>
+          <form action='' onSubmit={handleSubmit(onSubmit)}>
+            <div className='grid grid-cols-2 gap-3'>
+              <FormGroup>
+                <Label className={`${errors.title && 'text-red-400'}`}>Tiêu đề</Label>
+                <Input control={control} placeholder='Tiêu đề tin đăng' name='title' error={errors.title?.message} />
+              </FormGroup>
+              <FormGroup>
+                <Label>Danh mục</Label>
+                <Dropdown>
+                  <Dropdown.Select placeholder={categorySelected ? categorySelected.name : 'Chọn danh mục'} />
+                  <Dropdown.List>
+                    {categories?.map((item: Category) => (
+                      <Dropdown.Option option={item} key={item.id} onClick={handleClickOption}>
+                        {item.name}
+                      </Dropdown.Option>
+                    ))}
+                  </Dropdown.List>
+                  <span></span>
+                </Dropdown>
+              </FormGroup>
+              {auth?.roles.includes(Role.ADMIN || Role.MODERATOR) && (
+                <FormGroup>
+                  <Label>Trạng thái</Label>
+                  <div className='flex gap-x-6'>
+                    <Radio
+                      id='pending'
+                      control={control}
+                      name='status'
+                      value={PostStatus.PENDING}
+                      checked={watchStatus === PostStatus.PENDING}
+                    >
+                      Đang xử lý
+                    </Radio>
+                    <Radio
+                      id='approved'
+                      control={control}
+                      name='status'
+                      value={PostStatus.APPROVED}
+                      checked={watchStatus === PostStatus.APPROVED}
+                    >
+                      Đã duyệt
+                    </Radio>
+                    <Radio
+                      id='reject'
+                      control={control}
+                      name='status'
+                      value={PostStatus.REJECTED}
+                      checked={watchStatus === PostStatus.REJECTED}
+                    >
+                      Từ chối
+                    </Radio>
+                  </div>
+                </FormGroup>
+              )}
+              <FormGroup>
+                <Label className={`${errors.title && 'text-red-400'}`}>Giá bán</Label>
+                <Input
+                  control={control}
+                  placeholder='Giá của bạn'
+                  name='price'
+                  type='number'
+                  error={errors.price?.message}
+                ></Input>
+              </FormGroup>
+            </div>
+            <div className='flex items-center gap-x-3'>
+              <FormGroup>
+                <Label>Thumbnail</Label>
+                <Image to='post/thumbnail' handleChangeThumbnail={handleChangeThumbnail} />
+              </FormGroup>
+              <FormGroup>
+                <Label>Các ảnh khác</Label>
+                <div className='grid grid-cols-5'></div>
+                <ImageUpload
+                  to='post/imgs'
+                  multiple
+                  handleChangeImageUrls={handleChangeImageUrls}
+                  imageUrls={imageUrls}
+                />
+              </FormGroup>
+            </div>
             <FormGroup>
-              <Label>Trạng thái</Label>
-              <div className='flex gap-x-6'>
-                <Radio
-                  id='pending'
-                  control={control}
-                  name='status'
-                  value={PostStatus.PENDING}
-                  checked={watchStatus === PostStatus.PENDING}
-                >
-                  Đang xử lý
-                </Radio>
-                <Radio
-                  id='approved'
-                  control={control}
-                  name='status'
-                  value={PostStatus.APPROVED}
-                  checked={watchStatus === PostStatus.APPROVED}
-                >
-                  Đã duyệt
-                </Radio>
-                <Radio
-                  id='reject'
-                  control={control}
-                  name='status'
-                  value={PostStatus.REJECTED}
-                  checked={watchStatus === PostStatus.REJECTED}
-                >
-                  Từ chối
-                </Radio>
+              <Label>Nội dung</Label>
+              <div className='entry-content'>
+                <Editor
+                  apiKey={ENV.TINY_MCE_KEY}
+                  value={content}
+                  init={{
+                    height: 500,
+                    menubar: true,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'code',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table',
+                      'editimage',
+                      'wordcount'
+                    ],
+                    toolbar:
+                      'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                    images_upload_handler: handleImageUpload
+                  }}
+                  onEditorChange={handleEditorChange}
+                />
               </div>
             </FormGroup>
-          )}
-          <FormGroup>
-            <Label className={`${errors.title && 'text-red-400'}`}>Giá bán</Label>
-            <Input
-              control={control}
-              placeholder='Giá của bạn'
-              name='price'
-              type='number'
-              error={errors.price?.message}
-            ></Input>
-          </FormGroup>
-        </div>
-        <div className='flex items-center gap-x-3'>
-          <FormGroup>
-            <Label>Thumbnail</Label>
-            <Image to='post/thumbnail' handleChangeThumbnail={handleChangeThumbnail} />
-          </FormGroup>
-          <FormGroup>
-            <Label>Các ảnh khác</Label>
-            <div className='grid grid-cols-5'></div>
-            <ImageUpload to='post/imgs' multiple handleChangeImageUrls={handleChangeImageUrls} imageUrls={imageUrls} />
-          </FormGroup>
-        </div>
-        <FormGroup>
-          <Label>Nội dung</Label>
-          <div className='entry-content'>
-            <Editor
-              apiKey={ENV.TINY_MCE_KEY}
-              value={content}
-              init={{
-                height: 500,
-                menubar: true,
-                plugins: [
-                  'advlist',
-                  'autolink',
-                  'lists',
-                  'link',
-                  'image',
-                  'charmap',
-                  'preview',
-                  'anchor',
-                  'searchreplace',
-                  'visualblocks',
-                  'code',
-                  'fullscreen',
-                  'insertdatetime',
-                  'media',
-                  'table',
-                  'editimage',
-                  'wordcount'
-                ],
-                toolbar:
-                  'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-                images_upload_handler: handleImageUpload
-              }}
-              onEditorChange={handleEditorChange}
-            />
-          </div>
-        </FormGroup>
-        <Button
-          className='w-full max-w-[50%] mx-auto h-10'
-          type='submit'
-          // isloading={createPostMutation.isLoading}
-          disabled={!isValid}
-        >
-          Cập nhật tin đăng
-          {/* {auth?.roles.includes(Role.ADMIN) ? 'Đăng bài' : 'Gửi yêu cầu đăng bài'} */}
-        </Button>
-      </form>
+            <Button
+              className='w-full max-w-[50%] mx-auto h-10'
+              type='submit'
+              // isloading={createPostMutation.isLoading}
+              disabled={!isValid}
+            >
+              Cập nhật tin đăng
+            </Button>
+          </form>
+        </>
+      )}
     </>
   )
 }
