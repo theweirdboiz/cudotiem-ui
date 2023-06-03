248d1df86c5e4ed934971d922c18e56d7c97c1f9
theweirdboiz <19130227@st.hcmuaf.edu.vn>
2023-06-03 15:18:43 +0700

    test CRUD posts

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -52,3 +52,12 @@
 // }
 
 // export default ChatPage
+import React from 'react'
+
+type Props = {}
+
+const ChatPage = (props: Props) => {
+  return <div>ChatPage</div>
+}
+
+export default ChatPage
